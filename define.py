d1de9da654ac9b7cb35769d6accb0b15cea19b0f
Nguyen Bach Nhut Tien <tiennguyenbn@itrvn.com>
2024-06-07 10:09:49 +0700

    Update define.py

--- define.py
+++ define.py
@@ -1,7 +1,7 @@
 TEST_TIME = 'run-0'
 DATABASE = 'ltdb/'
 # DATABASE = 'mitdb/'
-PROJECT_DIR = '/media/dev7/Data_1/Workspace/trial_qrs_detection/'
+PROJECT_DIR = '/home/tien/Documents/ITR/itr-training-ecg-qrs_detection/'
 
 SAVE_MODEL_DIR = PROJECT_DIR + 'model/' + DATABASE
 TEMP_DIR = PROJECT_DIR + 'temp/'
@@ -10,16 +10,19 @@ RESULT_DIR = PROJECT_DIR + 'result/' + DATABASE
 
 
 LTDB_DIR = '/media/dev7/Data_1/Dataset/ltdb/'
-MITDB_DIR = '/media/dev7/Data_1/Dataset/mitdb/'
+# MITDB_DIR = '/home/tien/Documents/ITR/mit-bih-arrhythmia-database-1.0.0/'
+MITDB_DIR = '/home/tien/Documents/ITR/mit-bih-arrhythmia-database-1.0.0/'
 AHADB_DIR = '/media/dev7/Data_1/Dataset/ahadb/'
 ESCDB_DIR = '/media/dev7/Data_1/Dataset/escdb/'
 NSTDB_DIR = '/media/dev7/Data_1/Dataset/nstdb/'
 
-PREPROCESSED_DATA_DIR = '/media/dev7/Data_1/Dataset/trial_qrs_detection_dataset/' + DATABASE
-CHECK_POINT_DIR = '/media/dev7/Data_1/Dataset/trial_qrs_detection_dataset/checkpoint/' + DATABASE
+PREPROCESSED_DATA_DIR = '/media/tien/7E90428C90424ABB/ITR/preprocessed_v3/' + DATABASE
+CHECK_POINT_DIR = '/home/tien/Documents/ITR/checkpoint/' + DATABASE
 
-FREQUENCY_SAMPLING = 250
-NEIGHBOUR_POINT = int(FREQUENCY_SAMPLING * 0.1) + int(FREQUENCY_SAMPLING * 0.3) + 1
-POSITIVE_RANGE = int(FREQUENCY_SAMPLING * 0.04) + 1
+FREQUENCY_SAMPLING = 360
+NEIGHBOUR_POINT = int(FREQUENCY_SAMPLING * 0.1) + int(FREQUENCY_SAMPLING * 0.3) + 1 #  ~145
+NEIGHBOUR_PRE = int(FREQUENCY_SAMPLING * 0.1) # ~36
+NEIGHBOUR_POST = int(FREQUENCY_SAMPLING * 0.3) + 1 # ~108
+POSITIVE_RANGE = int(FREQUENCY_SAMPLING * 0.04) + 1 # ~15
 
 DATA_TYPE = 'float32'
